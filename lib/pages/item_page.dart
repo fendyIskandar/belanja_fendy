c8290300b0f6d9b8c66de515edb6b49ea1e4baea
fendyIskandar <fendyaremania87@gmail.com>
2022-03-22 14:14:20 -0700

    Navigasi and Route

---
+++ lib/pages/item_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:belanja_fendy/models/item.dart';
+
+class ItemPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Item itemArgs = ModalRoute.of(context).settings.arguments;
+    throw UnimplementedError();
+  }
+}
